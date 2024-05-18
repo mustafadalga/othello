6c3885b652f4defd2b386e4abc481d5ae8721cef
Mustafa Dalga <mustafadalgaa@gmail.com>
2024-05-18 12:00:31 +0300

    Write board creation utility

---
+++ app/_utilities/createBoard.ts
@@ -0,0 +1,36 @@
+import { Gamer } from "@/_enums";
+import { DIMENSION } from "@/_constants";
+import { Stones } from "@/_types";
+
+export default function createBoard(): Stones {
+    const initialStones = {
+        "33": Gamer.WHITE,
+        "34": Gamer.BLACK,
+        "43": Gamer.BLACK,
+        "44": Gamer.WHITE,
+    };
+
+    const board: Stones = [];
+    for (let row = 0; row < DIMENSION; row++) {
+        for (let col = 0; col < DIMENSION; col++) {
+            const property: string = row.toString() + col.toString()
+
+            if (Object.hasOwn(initialStones, property)) {
+                board.push({
+                    row,
+                    col,
+                    value: initialStones[property as keyof typeof initialStones] || null,
+                })
+                continue;
+            }
+
+            board.push({
+                row,
+                col,
+                value: initialStones[property as keyof typeof initialStones],
+            })
+        }
+    }
+
+    return board;
+}
