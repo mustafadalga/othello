2402d4568716aa854160562070c0762bd1b951e9
Mustafa Dalga <mustafadalgaa@gmail.com>
2024-05-19 15:28:22 +0300

    Write enums

---
+++ server/src/enums/index.ts
@@ -0,0 +1,10 @@
+export enum Gamer {
+    BLACK = "black",
+    WHITE = "white"
+}
+
+export enum GamerStatus {
+    CONNECTED = "connected",
+    DISCONNECTED = "disconnected",
+    EXITED = "exited"
+}
