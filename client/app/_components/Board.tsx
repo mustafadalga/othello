d6713f83217efa2c72cacf41092f118a429af60e
Mustafa Dalga <mustafadalgaa@gmail.com>
2024-06-15 13:16:13 +0300

    Add game playing functionality
    - Implemented rules for checking the winning player.
    - Added rules for checking no valid moves and changing move order.
    - Added functionality to finish the game if there are consecutive no moves.
    - Implemented fetching of updated game moves and game data during gameplay.
    - Set initial board state and dynamically assigned backend data from GraphQL to state.
    - Added user toast messages for various game events.

--- client/app/_components/Board.tsx
+++ client/app/_components/Board.tsx
@@ -1,59 +1,226 @@
-"use client";
 import { useCallback, useEffect, useState } from "react";
-import { Gamer } from "@/_enums";
+import { useParams } from "next/navigation";
+import { MutationFunction, useMutation, useQuery, useSubscription } from "@apollo/client";
+import { toast } from "react-toastify";
 import createBoard from "@/_utilities/createBoard";
-import { Stone, Stones } from "@/_types";
 import createHints from "@/_utilities/createHints";
 import reverseOpponentStones from "@/_utilities/reverseOpponentStones";
+import graphQLError from "@/_utilities/graphQLError";
+import { GET_GAME_BY_ID, GET_GAMERS_STONE_COUNT, GET_MOVES_BY_GAME_ID } from "@/_graphql/queries";
+import { CREATE_MOVES, UPDATE_GAME } from "@/_graphql/mutations";
+import { GAME_MOVED, GAME_RESTARTED, GAME_UPDATED, GAMERS_STONE_COUNT_UPDATED } from "@/_graphql/subscriptions";
+import getActiveGamerData, { IActiveGamerData } from "@/_utilities/getActiveGamerData";
+import useDeepCompareMemoize from "@/_hooks/useDeepCompareMemoize";
 import { DIMENSION } from "@/_constants";
+import { EGamer, LocalStorage } from "@/_enums";
+import {
+    IGame,
+    IGamersStoneCount,
+    IMove,
+    IMutationUpdateGame,
+    IMutationUpdateGameVariables,
+    Stone,
+    Stones,
+    SubscriptionGameMovedData,
+    SubscriptionGameRestartedData,
+    SubscriptionGamersStoneCountUpdatedData,
+    SubscriptionGameUpdatedData
+} from "@/_types";
 import Cell from "./Cell";
 
-export default function Board() {
-    const [ activeGamer, setActiveGamer ] = useState<Gamer>(Gamer.BLACK);
-    const [ noMoveCount, setNoMoveCount ] = useState<number>(0);
-    const [ board, setBoard ] = useState<Stones>(createBoard);
-    const opponent = activeGamer == Gamer.BLACK ? Gamer.WHITE : Gamer.BLACK;
-    const opponentStones = board.filter(cell => cell.gamer === opponent)
-    const hints = createHints(board, opponentStones, activeGamer);
-    const hasValidMove: boolean = !!hints.length
-    const hasConsecutiveNoMove: boolean = noMoveCount == 2;
 
-    const handleHint = useCallback((move: Stone) => {
-        const reversedStone = reverseOpponentStones(board, move, activeGamer);
-        const selectedCellIndex = move.row * DIMENSION + move.col
-        const reversedStoneIndex = [
-            ...reversedStone.map(item => item.row * DIMENSION + item.col),
-            selectedCellIndex
+export default function Board() {
+    const { id } = useParams()
+    const [ game, setGame ] = useState<IGame>()
+    const [ board, setBoard ] = useState<Stones>(createBoard);
+    const memoizedGame = useDeepCompareMemoize<IGame>(game as IGame);
+    const activeMoveOrder = useDeepCompareMemoize<IActiveGamerData>(getActiveGamerData(game as IGame));
+    const opponent = activeMoveOrder.gamer.color == EGamer.BLACK ? EGamer.WHITE : EGamer.BLACK;
+    const opponentStones = board.filter(cell => cell.gamer === opponent)
+    const hints = activeMoveOrder.isYourTurn ? createHints(board, opponentStones, activeMoveOrder.gamer.color) : [];
+    const hasValidMove: boolean = !!hints.length;
+    const isAllStoneReversed: boolean = board.every(cell => cell.gamer);
+
+    useQuery<{ game: IGame }>(GET_GAME_BY_ID, {
+        variables: {
+            id,
+        },
+        onCompleted: ({ game }) => {
+            if (game) {
+                setGame(game);
+            }
+        }
+    })
+
+    useQuery<{ moves: IMove[] }>(GET_MOVES_BY_GAME_ID, {
+        variables: {
+            gameID: id
+        },
+        onCompleted: ({ moves }) => {
+            if (moves) {
+                setBoard(prevState => prevState.map(cell => {
+                    const move = moves.find(move => move.row == cell.row && move.col == cell.col);
+                    return move ? move : cell;
+                }))
+            }
+        }
+    });
+
+    useQuery<{ game: IGamersStoneCount }>(GET_GAMERS_STONE_COUNT, {
+        variables: {
+            gameID: id,
+        },
+        onCompleted: ({ game }) => {
+            if (game) {
+                handleWinnerGamer(game, updateGame)
+            }
+        }
+    });
+
+    const [ createMove ] = useMutation(CREATE_MOVES, {
+        onError: graphQLError
+    });
+
+    const [ updateGame ] = useMutation<IMutationUpdateGame, IMutationUpdateGameVariables>(UPDATE_GAME, {
+        onError: graphQLError
+    });
+
+    useSubscription<SubscriptionGameUpdatedData>(GAME_UPDATED, {
+        variables: {
+            gameID: id as string
+        },
+        onData: ({ data: { data } }) => {
+            if (data?.game) {
+                setGame(data.game);
+            }
+        },
+        onError: graphQLError
+    });
+
+    useSubscription<SubscriptionGameMovedData>(GAME_MOVED, {
+        variables: {
+            gameID: id
+        },
+        onData: ({ data: { data } }) => {
+            if (data?.moves) {
+                const moves = data.moves;
+                setBoard(prevState => prevState.map(cell => {
+                    const move = moves.find(move => move.row == cell.row && move.col == cell.col);
+                    return move ? move : cell
+                }))
+            }
+        },
+        onError: graphQLError
+    });
+
+    useSubscription<SubscriptionGameRestartedData>(GAME_RESTARTED, {
+        variables: {
+            gameID: id
+        },
+        onData: ({ data: { data } }) => {
+            if (data?.game) {
+                setBoard(createBoard)
+            }
+        },
+        onError: graphQLError
+    });
+
+  useSubscription<SubscriptionGamersStoneCountUpdatedData>(GAMERS_STONE_COUNT_UPDATED, {
+        variables: {
+            gameID: id
+        },
+        onData: ({ data: { data } }) => {
+            if (data?.game) {
+                handleWinnerGamer(data.game, updateGame)
+            }
+        },
+        onError: graphQLError
+    });
+
+    const handleHint = useCallback(async (move: Stone) => {
+        if (!memoizedGame?.isGameStarted) {
+            return toast.info("Game has not started yet!");
+        }
+        if (memoizedGame.isGameFinished) {
+            return toast.info("Game has finished!");
+        }
+        if (!activeMoveOrder.isYourTurn) {
+            return toast.warn("It's not your turn!");
+        }
+
+
+        const reversedStone: Stone[] = reverseOpponentStones(board, move, activeMoveOrder.gamer.color);
+        const moves = [
+            {
+                ...move,
+                gamer: activeMoveOrder.gamer.color,
+                gameID: id
+            },
+            ...(reversedStone.map(stone => ({
+                ...stone,
+                gamer: activeMoveOrder.gamer.color,
+                gameID: id
+            })))
         ];
 
-        setBoard(prevState => {
-            return prevState.map((cell, index) => {
-                if (reversedStoneIndex.includes(index)) {
-                    return {
-                        ...cell,
-                        gamer: activeGamer
+        createMove({
+            variables: {
+                moves
+            }
+        });
+
+    }, [ memoizedGame, board, activeMoveOrder ]);
+
+
+    // handle no valid move and consecutive moves
+    useEffect(() => {
+        if (!
+                (memoizedGame?.isGameStarted == true &&
+                    memoizedGame?.isGameFinished == false)
+            || !activeMoveOrder.isYourTurn
+            || isAllStoneReversed) return;
+        const updatedGamers = memoizedGame.gamers.map(gamer => {
+            if (gamer.id == activeMoveOrder.gamer.id) {
+                return {
+                    ...gamer,
+                    canMove: false
+                }
+            }
+            return gamer
+        });
+
+        const hasConsecutiveNoMove: boolean = updatedGamers.every(gamer => !gamer.canMove);
+
+        if (hasConsecutiveNoMove) {
+            updateGame({
+                variables: {
+                    data: {
+                        _id: id as string,
+                        isGameFinished: true,
+                        moveOrder: null,
+                        gamers: updatedGamers
                     }
                 }
-                return cell;
             })
-        })
-
-        setActiveGamer(opponent)
-    }, [ activeGamer, board, opponent ]);
-
-    useEffect(() => {
-        if (hasConsecutiveNoMove) {
-            console.log("there is consecutive noMove, game over");
-            return;
+            toast.info("There is consecutive noMove. Game is tie!", {
+                toastId: "consecutiveNoMove"
+            })
+        } else if (!hasValidMove) {
+            updateGame({
+                variables: {
+                    data: {
+                        _id: id as string,
+                        moveOrder: nextMoveOrderID(memoizedGame as IGame),
+                        gamers: updatedGamers
+                    }
+                }
+            })
+            toast.info("There is no valid move. So it turn to rival gamer!", {
+                toastId: "noValidMove"
+            })
         }
-        if (!hasValidMove) {
-            console.log("there is no move , so it turn to rival gamer");
-            setActiveGamer(opponent);
-            setNoMoveCount(prevState => prevState + 1);
-        }
 
-    }, [ hasConsecutiveNoMove, hasValidMove, opponent ])
-
+    }, [ memoizedGame, activeMoveOrder, hasValidMove, isAllStoneReversed ])
 
     return (
         <div className="grid grid-cols-8 grid-rows-8 max-w-xl mx-4 sm:max-0 bg-[#038947] border border-gray-900">
@@ -62,4 +229,54 @@ export default function Board() {
                                            stone={c}/>)}
         </div>
     )
+}
+
+function nextMoveOrderID(game: IGame): string {
+    if (game) {
+        if (game.moveOrder === game.gamers[0].id) {
+            return game.gamers[1].id
+        } else {
+            return game.gamers[0].id
+        }
+    }
+    return "";
+}
+
+function handleWinnerGamer(gamersStoneCount: IGamersStoneCount, updateGame: MutationFunction<IMutationUpdateGame, IMutationUpdateGameVariables>) {
+    const MAX_STONE_COUNT: number = DIMENSION * DIMENSION;
+    const isAllStoneReversed: boolean = gamersStoneCount.count.BLACK + gamersStoneCount.count.WHITE == MAX_STONE_COUNT;
+
+    if (!isAllStoneReversed) return;
+
+    let message: string = "";
+    const whiteGamerID: string = gamersStoneCount.game.gamers.find(gamer => gamer.color == EGamer.WHITE)?.id!;
+    const blackGamerID: string = gamersStoneCount.game.gamers.find(gamer => gamer.color == EGamer.BLACK)?.id!;
+    const userID: string = localStorage.getItem(LocalStorage.USERID)!;
+    let winnerID: string | null = "";
+
+    if (gamersStoneCount.count.BLACK > gamersStoneCount.count.WHITE) {
+        message = blackGamerID == userID ? "Game finished! You won! 🎉" : "Game finished! You lost! 😢";
+        winnerID = blackGamerID;
+    } else if (gamersStoneCount.count.WHITE > gamersStoneCount.count.BLACK) {
+        message = whiteGamerID == userID ? "Game finished! You won! 🎉" : "Game finished! You lost! 😢"
+        winnerID = whiteGamerID;
+    } else {
+        message = "Game finished! It's a tie! 🤝"
+    }
+
+    updateGame({
+        variables: {
+            data: {
+                _id: gamersStoneCount.game._id,
+                isGameStarted: false,
+                isGameFinished: true,
+                moveOrder: null,
+                winnerGamer: winnerID,
+            }
+        }
+    })
+
+    toast.success(message, {
+        toastId: message
+    });
 }
