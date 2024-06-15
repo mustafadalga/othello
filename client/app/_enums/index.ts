24ba3b2e6028a7b12c56c376d0d54e9a213f0dd3
Mustafa Dalga <mustafadalgaa@gmail.com>
2024-06-15 12:36:29 +0300

    Structure client enums and types

--- client/app/_enums/index.ts
+++ client/app/_enums/index.ts
@@ -1,8 +1,24 @@
-export enum Gamer {
+export enum EGamer {
     BLACK = "BLACK",
     WHITE = "WHITE"
 }
 
 export enum LocalStorage {
-    USERID = "userID"
+    USERID = "userID",
+    GAME_STARTED_MESSAGE_SHOWN = "gameStartedMessageShown",
+}
+
+export enum GamerStatus {
+    CONNECTED = "CONNECTED",
+    DISCONNECTED = "DISCONNECTED",
+    EXITED = "EXITED"
+}
+
+
+export enum ErrorCode {
+    GAME_NOT_FOUND = 'GAME_NOT_FOUND',
+    GAME_FULL = 'GAME_FULL',
+    INTERNAL_SERVER_ERROR = 'INTERNAL_SERVER_ERROR',
+    BAD_USER_INPUT = "BAD_USER_INPUT",
+    DUPLICATE_MOVE = "DUPLICATE_MOVE"
 }
--- client/app/_types/index.ts
+++ client/app/_types/index.ts
@@ -1,10 +1,79 @@
-import { Gamer } from "@/_enums";
+import { EGamer, GamerStatus } from "@/_enums";
 
 export type Stone = {
     row: number,
     col: number,
-    gamer: Gamer | null
+    gamer: EGamer | null
+}
+
+export type Stones = Stone[]
+
+export interface IGamer {
+    id: string
+    color: EGamer
+    status: GamerStatus,
+    canMove: boolean
+}
+
+export interface IGame {
+    _id: string,
+    gamers: IGamer[]
+    isGameFinished: boolean
+    isGameStarted: boolean
+    moveOrder: string
+    winnerGamer: string
+    playAgainstComputer: boolean,
+    exitGamer: string
+}
+
+export interface IMove {
+    row: number
+    col: number
+    gamer: EGamer,
+    gameID: string
 }
 
 
-export type Stones = Stone[]
+export type IGamersStoneCount = {
+    game: {
+        _id: string,
+        gamers: IGamer[],
+    },
+    count: {
+        [key in EGamer]: number
+    }
+}
+
+export interface SubscriptionGameMovedData {
+    moves: IMove[] | undefined
+}
+
+export interface SubscriptionGameUpdatedData {
+    game: IGame | undefined
+}
+
+export interface SubscriptionGameRestartedData {
+    game: IGame | undefined
+}
+
+export interface SubscriptionGamersStoneCountUpdatedData {
+    game: IGamersStoneCount | undefined
+}
+
+export interface IMutationUpdateGameVariables {
+    data: {
+        _id: string,
+        isGameStarted?: boolean
+        isGameFinished?: boolean
+        moveOrder: string | null
+        winnerGamer?: string
+        exitGamer?: string
+        gamers?: IGamer[]
+    }
+}
+
+export interface IMutationUpdateGame {
+    game: {
+        _id: string
+    }
+}
