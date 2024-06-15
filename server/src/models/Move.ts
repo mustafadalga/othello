0a466e2b72189730989a7ecba79e62b51546aa15
Mustafa Dalga <mustafadalgaa@gmail.com>
2024-06-15 12:43:26 +0300

    Refactore enum name

--- server/src/models/Move.ts
+++ server/src/models/Move.ts
@@ -1,11 +1,11 @@
 import { Schema, model } from "mongoose";
 import mongoose from "mongoose";
-import { Gamer } from "@/enums";
+import { EGamer } from "@/enums";
 
 const schema = new mongoose.Schema({
     row: { type: Number, required: true },
     col: { type: Number, required: true },
-    gamer: { type: String, enum: Gamer, required: true },
+    gamer: { type: String, enum: EGamer, required: true },
     gameID: { type: Schema.Types.ObjectId, ref: 'Game', required: true },
 });
 schema.index({ row: 1, col: 1, gameID: 1 }, { unique: true });
