f0bc084280ad66388b1b3feed344d38268a6e3d3
Mustafa Dalga <mustafadalgaa@gmail.com>
2024-06-15 12:24:44 +0300

    Add active user ui sign

--- client/app/_components/Stone.tsx
+++ client/app/_components/Stone.tsx
@@ -1,12 +1,12 @@
-import { Gamer } from "@/_enums";
+import { EGamer } from "@/_enums";
 
-export default function Stone({ gamer }: { gamer: Gamer }) {
-    const bgColor = gamer == Gamer.BLACK ? "bg-[radial-gradient(circle_at_30%_30%,rgba(105,105,105),rgba(0,0,0)_70%)]" : "bg-[radial-gradient(circle_at_30%_30%,rgb(255,255,255),rgb(230,230,230)_70%)]"
+export default function Stone({ gamer, isActive }: { gamer: EGamer, isActive?: boolean }) {
+    const bgColor = gamer == EGamer.BLACK ? "bg-[radial-gradient(circle_at_30%_30%,rgba(105,105,105),rgba(0,0,0)_70%)]" : "bg-[radial-gradient(circle_at_30%_30%,rgb(255,255,255),rgb(230,230,230)_70%)]"
 
     return (
         <div
             className={`${bgColor} grid place-items-center w-6 h-6 lg:w-8 lg:h-8 xl:w-10 xl:h-10 rounded-full border border-solid border-gray-300 shadow-[5px_5px_15px_rgba(0,0,0,0.3),inset_2px_2px_5px_rgba(255,255,255,0.1),inset_-2px_-2px_5px_rgba(0,0,0,0.4)]`}>
-            <div className="bg-green-600 p-1 rounded-full"></div>
+            <div className={`p-0.5 sm:p-1 rounded-full ${isActive ? 'bg-green-600' : ''}`}></div>
         </div>
     )
 }
