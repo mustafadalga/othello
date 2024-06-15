cb361cddfdf03d529adc51ba293f8a963dcf2061
Mustafa Dalga <mustafadalgaa@gmail.com>
2024-06-15 12:43:07 +0300

    Remove playAgainstComputer: property and separate GamerSchema

--- server/src/models/Game.ts
+++ server/src/models/Game.ts
@@ -1,17 +1,20 @@
 import { Schema, model } from "mongoose";
-import { Gamer, GamerStatus } from "@/enums";
+import { EGamer, GamerStatus } from "@/enums";
+
+const GamerSchema = new Schema({
+    id: { type: String, required: true },
+    color: { type: String, enum: EGamer, required: true },
+    status: { type: String, enum: GamerStatus, required: true, default: GamerStatus.CONNECTED },
+    canMove: { type: Boolean, default: true },
+}, { _id: false });
+
 
 const schema = new Schema({
-    gamers: [ {
-        id: { type: String, required: true },
-        color: { type: String, enum: Gamer, required: true },
-        status: { type: String, enum: GamerStatus, required: true, default: GamerStatus.CONNECTED }
-    } ],
+    gamers: [ GamerSchema],
     isGameFinished: { type: Boolean, default: false },
     isGameStarted: { type: Boolean, default: false },
     moveOrder: { type: String, default: null },
     winnerGamer: { type: String, default: null },
-    playAgainstComputer: { type: Boolean, default: false },
     moves: [ { type: Schema.Types.ObjectId, ref: 'Move' } ],
     exitGamer: { type: String, default: null },
 });
