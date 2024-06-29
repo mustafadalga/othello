e914d970708d8308adfc19b437aa75954c18a13d
Mustafa Dalga <mustafadalgaa@gmail.com>
2024-06-29 10:55:25 +0300

    Change hint border color to gradient for enhanced visual effect

--- client/app/_components/Hint.tsx
+++ client/app/_components/Hint.tsx
@@ -7,15 +7,15 @@ const scaleInVariants = {
 };
 
 export default function Hint({ activeGamer, isHintClicked }: { activeGamer: EGamer, isHintClicked: boolean }) {
-    const clickedClassName: string = activeGamer == EGamer.BLACK ?
-        "before:stone-front after:stone-front-inner" : "before:stone-back after:stone-back-inner";
-    const defaultClassName: string = (activeGamer == EGamer.BLACK ? "before:group-hover:bg-black/20 before:border-black" : "before:group-hover:bg-white/20 before:border-white") + " before:border before:border-solid ";
+    const clickedBaseClassName: string = "backface-hidden before:block before:absolute before:rounded-full before:inset-[10%] after:block after:absolute after:rounded-full after:inset-0 after:m-[25%]";
+    const clickedClassName: string = (activeGamer == EGamer.BLACK ? "before:stone-front after:stone-front-inner" : "before:stone-back after:stone-back-inner") + " " + clickedBaseClassName;
+    const defaultClassName: string = (activeGamer == EGamer.BLACK ? "before:from-[#333] before:to-[#888] before:[-webkit-mask:linear-gradient(#fff_0_0)_content-box,linear-gradient(#fff_0_0)]" : "before:from-white before:to-[#e1e1e1] before:[-webkit-mask:linear-gradient(#fff_0_0)_content-box,linear-gradient(#fff_0_0)]") + " before:bg-gradient-to-b before:p-0.5 before:content-[''] before:[mask-composite:exclude]";
     const className = `${isHintClicked ? clickedClassName : defaultClassName}`
 
     return (
         <motion.div
             key={isHintClicked.toString()}
-            className={`${className} absolute w-full h-full backface-hidden before:block before:absolute before:rounded-full before:inset-[10%] after:block after:absolute after:rounded-full after:inset-0 after:m-[25%]`}
+            className={`${className} absolute w-full h-full backface-hidden before:block before:absolute before:rounded-full before:inset-[10%] before:safari-hint-inset after:block after:absolute after:rounded-full after:inset-0 after:m-[25%] `}
             variants={scaleInVariants}
             initial="hidden"
             animate="visible"
